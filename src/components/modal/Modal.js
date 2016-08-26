afa2dd5ee20ba0924b1ef8e8f163b1cee18022b6
Dan Caragea <dancaragea@gmail.com>
2016-08-26 20:32:24 +0300

    close modal on ESC

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,23 +1,53 @@
-import React, { PropTypes } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import classNames from 'classnames';
+import addEventListener from '../../utils/addEventListener';
+import ownerDocument from '../../utils/ownerDocument';
 
-function Modal({ children, className, isActive, hasClose, hasBgClose, onClose }) {
-  return (
-    <div className={classNames('modal', className, isActive && 'is-active')}>
-      <div className="modal-background" onClick={hasBgClose && onClose} />
-      {children}
-      {hasClose && <button className="modal-close" onClick={onClose} /> }
-    </div>
-  );
+class Modal extends PureComponent {
+  static propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string,
+    isActive: PropTypes.bool,
+    hasClose: PropTypes.bool,
+    hasBgClose: PropTypes.bool,
+    onClose: PropTypes.func,
+  };
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.isActive && nextProps.isActive) {
+      this.onShow();
+    }
+  }
+
+  handleDocumentKeyUp = (e) => {
+    if (e.keyCode === 27) {
+      this.onClose();
+    }
+  };
+
+  onShow = () => {
+    const doc = ownerDocument(this);
+    this._onDocumentKeyupListener = addEventListener(doc, 'keyup', this.handleDocumentKeyUp);
+  };
+
+  onClose = () => {
+    this._onDocumentKeyupListener.remove();
+    if (this.props.onClose) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { children, className, isActive, hasClose, hasBgClose } = this.props;
+
+    return (
+      <div className={classNames('modal', className, isActive && 'is-active')}>
+        <div className="modal-background" onClick={hasBgClose && this.onClose} />
+        {children}
+        {hasClose && <button className="modal-close" onClick={this.onClose} /> }
+      </div>
+    );
+  }
 }
-
-Modal.propTypes = {
-  children: PropTypes.any,
-  className: PropTypes.string,
-  isActive: PropTypes.bool,
-  hasClose: PropTypes.bool,
-  hasBgClose: PropTypes.bool,
-  onClose: PropTypes.func,
-};
 
 export default Modal;
